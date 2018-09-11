fb25c3899ed0d43bcd025c464a5d72d8777fe471
Anel Guerrero <anelguerrero23@gmail.com>
2018-09-11 16:51:14 -0400

    Connect root.jsx to browser

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as APIUtil from './util/session_api_util';
 import { configureStore } from './store/store';
-
+import Root from './components/root';
 window.login = APIUtil.login;
 window.signup = APIUtil.signup;
 window.logout = APIUtil.logout;
@@ -11,5 +11,5 @@ window.logout = APIUtil.logout;
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Code</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
