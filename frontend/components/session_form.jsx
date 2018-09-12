661c40b698361a3485816739e340c64826eff34d
Anel Guerrero <anelguerrero23@gmail.com>
2018-09-12 14:01:09 -0400

    Fix session form jsx tags

--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -44,36 +44,38 @@ class SessionForm extends React.Component {
     return (
       <div id="content" >
         <nav className='session-nav'>
-      <div>
-        <nav>
-          <Link to={`/login`}>Log In</Link>
-          <Link to={`/signup`}>Sign Up</Link>
-        </nav>
-        <div>
-          "Create your <strong>code</strong> account. It's free and only takes a minute."
-        </div>
-        <form>
+          <div>
+            <nav>
+              <Link to={`/login`}>Log In</Link>
+              <Link to={`/signup`}>Sign Up</Link>
+            </nav>
+            <div>
+              "Create your <strong>code</strong> account. It's free and only takes a minute."
+            </div>
+            <form>
 
-          <label>Display Name
-            <input
-              type='text'
-              value={this.state.username}
-              onChange={this.updateField('username')}/>
-          </label>
-          <label>Email(required, but never shown)
-            <input
-              type='text'
-              value={this.state.email}
-              onChange={this.updateField('email')}/>
-          </label>
-          <label>Password
-            <input
-              type='password'
-              value={this.state.password}
-              onChange={this.updateField('password')}/>
-          </label>
-          <button onClick={this.handleSubmit.bind(this)}>{this.props.formType}</button>
-        </form>
+              <label>Display Name
+                <input
+                  type='text'
+                  value={this.state.username}
+                  onChange={this.updateField('username')}/>
+              </label>
+              <label>Email(required, but never shown)
+                <input
+                  type='text'
+                  value={this.state.email}
+                  onChange={this.updateField('email')}/>
+              </label>
+              <label>Password
+                <input
+                  type='password'
+                  value={this.state.password}
+                  onChange={this.updateField('password')}/>
+              </label>
+              <button onClick={this.handleSubmit.bind(this)}>{this.props.formType}</button>
+            </form>
+          </div>
+        </nav>
       </div>
     );
   }
