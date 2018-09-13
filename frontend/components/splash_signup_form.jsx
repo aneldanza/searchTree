7a7eac6721bb64afe21317f70123db97e9b55242
Anel Guerrero <anelguerrero23@gmail.com>
2018-09-13 17:13:04 -0400

    Center the splash sign up form on the page properly

--- frontend/components/splash_signup_form.jsx
+++ frontend/components/splash_signup_form.jsx
@@ -64,58 +64,60 @@ class SplashSignup extends React.Component {
   render() {
 
     return(
-      <div id='splash-signup-container'>
-        <section className='welcome-text'>
-          <h1>Learn, Share, Build</h1>
-          <p>Come to <strong>code</strong> to learn, share their knowledge, and build their careers.</p>
-          <p>
-            Join the world’s largest developer community.
-          </p>
-        </section>
+      <section className='splash-welcome'>
+        <div id='splash-signup-container'>
+          <section className='welcome-text'>
+            <h1>Learn, Share, Build</h1>
+            <p>Come to <strong>code</strong> to learn, share their knowledge, and build their careers.</p>
+            <p>
+              Join the world’s largest developer community.
+            </p>
+          </section>
 
-        <form className='splash-form-container'>
-          <div className='label-input'>
-            <label className='splash-label'>Display Name</label>
-            <input
-              className='splash-input'
-              placeholder='J.Doe'
-              type='text'
-              value={this.state.username}
-              onChange={this.updateField('username')}/>
-            {this.userErrors()}
-          </div>
-
-          <div className='label-input'>
-            <label className='splash-label'>Email address</label>
-            <input
-              className='splash-input'
-              placeholder='you@example.org'
-              type='text'
-              value={this.state.email}
-              onChange={this.updateField('email')}/>
-            {this.emailErrors()}
-          </div>
-
-          <div className='label-input'>
-            <label className='splash-label'>Password</label>
-            <input
-              className='splash-input'
-              placeholder='*******'
-              type='password'
-              value={this.state.password}
-              onChange={this.updateField('password')}/>
-            {this.passwordErrors()}
-          </div>
-
-          <div className='label-input' id='button-group'>
-            <div id='splash-signup-button'>
-              <button onClick={this.handleSubmit.bind(this)}>{this.props.formType}</button>
+          <form className='splash-form-container'>
+            <div className='label-input'>
+              <label className='splash-label'>Display Name</label>
+              <input
+                className='splash-input'
+                placeholder='J.Doe'
+                type='text'
+                value={this.state.username}
+                onChange={this.updateField('username')}/>
+              {this.userErrors()}
             </div>
 
-          </div>
+            <div className='label-input'>
+              <label className='splash-label'>Email address</label>
+              <input
+                className='splash-input'
+                placeholder='you@example.org'
+                type='text'
+                value={this.state.email}
+                onChange={this.updateField('email')}/>
+              {this.emailErrors()}
+            </div>
 
-        </form>
-      </div>
+            <div className='label-input'>
+              <label className='splash-label'>Password</label>
+              <input
+                className='splash-input'
+                placeholder='*******'
+                type='password'
+                value={this.state.password}
+                onChange={this.updateField('password')}/>
+              {this.passwordErrors()}
+            </div>
+
+            <div className='label-input' id='button-group'>
+              <div id='splash-signup-button'>
+                <button onClick={this.handleSubmit.bind(this)}>{this.props.formType}</button>
+              </div>
+
+            </div>
+
+          </form>
+        </div>
+      </section>
     );
   }
 }
