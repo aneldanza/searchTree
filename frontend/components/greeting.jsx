01d7e568f8b8e101dd82992234bb8e7a02d417a3
Anel Guerrero <anelguerrero23@gmail.com>
2018-09-13 19:37:55 -0400

    Add logo to the header and icon to the side bar

--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -2,28 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const Greeting = (props) => {
+  let buttons_section;
     if (props.currentUser) {
-
-      return (
-        // <nav className='main-header'>
-          <nav className='header-right'>
-            <div className='inner-header'>
-              <p>Hello, {props.currentUser.username}!</p>
-              <button id='cool-button' onClick={() => props.logout()}>Log Out</button>
-            </div>
-          </nav>
-        // </nav>
+      buttons_section = (
+        <div id='buttons-section'>
+          <p>Hello, {props.currentUser.username}!</p>
+          <button id='cool-button' onClick={() => props.logout()}>Log Out</button>
+        </div>
       );
     } else {
-      return (
-      // <nav className='main-header'>
-        <nav className='header-right'>
+      buttons_section = (
+        <div id='buttons-section'>
           <Link to={`/login`} id='login'>Log In</Link>
           <Link to={`/signup`} id='cool-button'>Sign Up</Link>
           <button id='cool-button' onClick={() => props.demoLogin()}>Demo LogIn</button>
-        </nav>
-      // </nav>
+        </div>
       );
     }
 
+  return (
+    <nav className='header-right'>
+      <div className='inner-header'>
+        <span id="logo">search<strong>Tree</strong></span>
+        <input id='search-bar' type='text'/>
+        {buttons_section}
+      </div>
+    </nav>
+  );
 };
