fa0abb0f9ac7d4ada6d0873d50ea6c398284dd83
Anel Guerrero <anelguerrero23@gmail.com>
2018-09-14 10:52:33 -0400

    Add questions_api_util

---
+++ frontend/util/quesions_api_util.js
@@ -0,0 +1,37 @@
+export const fetchAllQuestions = () => {
+  return $.ajax({
+    method: "GET",
+    url: 'api/questions',
+  });
+};
+
+export const fetchQuestion = (id) => {
+  return $.ajax({
+    method: "GET",
+    url: `api/questions/${id}`,
+  });
+};
+
+export const createQuestion = (question) => {
+  return $.ajax({
+    method: "POST",
+    url: `api/questions/`,
+    data: {question: question},
+  });
+};
+
+export const updateQuestion = (question) => {
+  return $.ajax({
+    method: "PATCH",
+    url: `api/questions/${question.id}`,
+    data: {question: question},
+  });
+};
+
+export const deleteQuestion = (id) => {
+  return $.ajax({
+    method: "DELETE",
+    url: `api/questions/${id}`,
+  });
+};
+
