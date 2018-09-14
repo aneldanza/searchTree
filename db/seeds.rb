74c6c907f4648c45acfe5c21b2bc9b9769898d62
Anel Guerrero <anelguerrero23@gmail.com>
2018-09-14 12:09:30 -0400

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Question.destroy_all
 
 User.create({username: 'demo', password: 'hunter2', email: 'email@email.com'});
 
 
 Question.create({title: "How to enable mixed content in Chrome using OS-X?",
-body: "I need to unblock mixed content to view an un-secure webpage on secured website. I cant find a way to do that. If I click shield or lock icon it doesn't give me an option to load the content. I'm using OS-X. ",
-user_id: User.first.id})
+body: "I need to unblock mixed content to view an un-secure webpage on secured website. I cant find a way to do that. If I click shield or lock icon it doesn't give me an option to load the content. I'm using OS-X. ", user_id: User.first.id})
