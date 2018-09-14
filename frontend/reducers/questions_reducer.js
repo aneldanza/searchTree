ba3b4ce9c272059a247ae6e40b037cec3c1fd550
Anel Guerrero <anelguerrero23@gmail.com>
2018-09-14 13:52:04 -0400

    Add quesitons_reducer
    Also add questionsReducer to entitities reducer

---
+++ frontend/reducers/questions_reducer.js
@@ -0,0 +1,18 @@
+import {RECEIVE_ALL_QUESTIONS, RECEIVE_QUESTION, DELETE_QUESTION} from '../actions/questions_actions';
+import merge from 'lodash/merge';
+
+export const questionsReducer = (state = {}, action) => {
+  Object.freeze();
+  switch(action.type) {
+    case RECEIVE_ALL_QUESTIONS:
+      return action.questions;
+    case RECEIVE_QUESTION: 
+      return merge({}, state, { [action.question.id]: action.question });
+    case DELETE_QUESTION:
+      const newState = merge({}, state);
+      delete newState[action.questionID];
+      return newState;
+    default:
+      return state;
+  }
+};
