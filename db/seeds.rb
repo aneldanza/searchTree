6f6fd6582b1ccfb65fc6721ac97baf2af81906b7
Anel Guerrero <anelguerrero23@gmail.com>
2018-09-17 15:56:05 -0400

    Fix signup errors issue and seed db with one more question and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# User.destroy_all
+User.destroy_all
 Question.destroy_all
 
 User.create({username: 'demo', password: 'hunter2', email: 'email@email.com'});
+User.create({username: 'Bill', password: 'hunter2', email: 'bill@email.com'});
 
 
 Question.create({title: "How to enable mixed content in Chrome using OS-X?",
 body: "I need to unblock mixed content to view an un-secure webpage on secured website. I cant find a way to do that. If I click shield or lock icon it doesn't give me an option to load the content. I'm using OS-X. ", user_id: User.first.id})
+
+Question.create({
+  title: "How to implement third-party impressions tracking URL from Sizmek?",
+  body: "I've received 2 third-party tracking links for an Expanding banner ad. One of them is for Click Tracking and another is for Imps tracking.",
+  user_id: User.second.id
+})
