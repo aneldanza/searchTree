9b843dbaf1fe07db8ae83b4e1dfd98ad67829b4f
Anel Guerrero <anelguerrero23@gmail.com>
2018-09-18 09:30:57 -0400

    Add Protected route to ask question page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GreetingContainer from './greeting_container';
 import LoginFormContainer from './login_form_container';
 import SignupFormContainer from './signup_form_container';
-import { AuthRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import SideBar from './side_bar';
 import { Route, Switch } from 'react-router-dom';
 import HomePageContainer from './home_page/home_page_container';
@@ -23,7 +23,7 @@ export const App = () => {
         </aside>
         <main>
         <Switch>
-          <Route exact path='/questions/ask' component={QuestionForm} />
+          <ProtectedRoute exact path='/questions/ask' component={QuestionForm} />
           <AuthRoute exact path='/login' component={LoginFormContainer} />
           <AuthRoute exact path='/signup' component={SignupFormContainer} />
           <Route exact path='/' component={QuestionsIndexContainer} />
