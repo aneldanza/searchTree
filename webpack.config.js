e6ae3b40c8864fdd0b9f9c19029fd20b91915ab0
Anel Guerrero <anelguerrero23@gmail.com>
2018-10-10 20:56:35 -0400

    Add uglifyjs-webpack-plugin and change optimitzation for it in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -11,60 +11,64 @@ var prodPlugins = [
       'NODE_ENV': JSON.stringify('production')
     }
   }),
+  {
+    optimization: {
+      minimizer: [new UglifyJsPlugin()]
+    }
+  }
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+  )
+  
+  module.exports = {
+    context: __dirname,
+    entry: './frontend/index.jsx',
+    output: {
+      path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+      filename: 'bundle.js'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '*']
+    },
+    plugins: plugins,
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            query: {
+              presets: ['@babel/env', '@babel/react']
+            }
+          },
+        }
+      ]
+    },
+    devtool: 'source-map'
+  };
+  
   // new webpack.optimize.UglifyJsPlugin({
   //   compress: {
   //     warnings: true
   //   }
   // })
-  {
-    optimization: {
-      minimizer: [
-        // we specify a custom UglifyJsPlugin here to get source maps in production
-        new UglifyJsPlugin({
-          cache: true,
-          parallel: true,
-          uglifyOptions: {
-            compress: false,
-            ecma: 6,
-            mangle: true
-          },
-          sourceMap: true
-        })
-      ]
-    }
-  }
-];
-
-
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-)
-
-module.exports = {
-  context: __dirname,
-  entry: './frontend/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
-  },
-  plugins: plugins,
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        },
-      }
-    ]
-  },
-  devtool: 'source-map'
-};
+  // {
+  //   optimization: {
+  //     minimizer: [
+  //       // we specify a custom UglifyJsPlugin here to get source maps in production
+  //       new UglifyJsPlugin({
+  //         cache: true,
+  //         parallel: true,
+  //         uglifyOptions: {
+  //           compress: false,
+  //           ecma: 6,
+  //           mangle: true
+  //         },
+  //         sourceMap: true
+  //       })
+  //     ]
+  //   }
+  // }
