7867e75e340c0966bbbdd8a438782225cee63283
Anel Guerrero <anelguerrero23@gmail.com>
2018-10-18 21:18:19 -0400

    Create comments migration

---
+++ db/migrate/20181019010939_create_comments.rb
@@ -0,0 +1,14 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.text :body, null: false
+      t.integer :user_id, null: false
+      t.integer :post_id, null: false
+      t.string :post_type, null: false
+      t.index :post_id
+      t.index :post_type
+      t.index :user_id
+      t.timestamp
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_223315) do
+ActiveRecord::Schema.define(version: 2018_10_19_010939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2018_09_20_223315) do
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.string "post_type", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["post_type"], name: "index_comments_on_post_type"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
