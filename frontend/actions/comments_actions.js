777293468de08e38c34b97f21a805f05eca1024b
Anel Guerrero <anelguerrero23@gmail.com>
2018-10-18 21:54:44 -0400

    Add comments actions

---
+++ frontend/actions/comments_actions.js
@@ -0,0 +1,19 @@
+import * as CommentAPIUtil from '../util/comments_api_util';
+import { receiveQuestion } from '../actions/questions_actions'
+import { receiveAnswer } from '../actions/answer_actions';
+import { receiveErrors } from '../actions/session_actions';
+
+export const createComment = (comment) => {
+  return dispatch => {
+    CommentAPIUtil.createComment(comment) 
+    .then( (post) => {
+      if (comment.post_type === 'Question') {
+        return dispatch(receiveQuestion(post));
+      } else {
+        return dispatch(receiveAnswer(post));
+      }
+    }, errors => {
+      dispatch(receiveErrors(errors.responseJSON));
+    })
+  }
+}
