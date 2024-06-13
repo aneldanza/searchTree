4ca038147881507cd52329a33dd925cb01c905b0
Anel Danza <aneldanza23@gmail.com>
2024-06-13 13:22:46 -0400

    Update versions of gems and ruby

--- db/migrate/20181024192751_add_timestamps_to_comments_table.rb
+++
@@ -1,7 +0,0 @@
-class AddTimestampsToCommentsTable < ActiveRecord::Migration[5.2]
-  def change
-    add_timestamps :comments, default: DateTime.now
-    change_column_default :comments, :created_at, nil
-    change_column_default :comments, :updated_at, nil
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_062147) do
-
+ActiveRecord::Schema[7.1].define(version: 2018_11_14_062147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +18,8 @@ ActiveRecord::Schema.define(version: 2018_11_14_062147) do
     t.text "body", null: false
     t.integer "user_id", null: false
     t.integer "question_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "tags"
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
@@ -31,8 +30,8 @@ ActiveRecord::Schema.define(version: 2018_11_14_062147) do
     t.integer "user_id", null: false
     t.integer "post_id", null: false
     t.string "post_type", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["post_type"], name: "index_comments_on_post_type"
     t.index ["user_id"], name: "index_comments_on_user_id"
@@ -42,8 +41,8 @@ ActiveRecord::Schema.define(version: 2018_11_14_062147) do
     t.string "title", null: false
     t.text "body", null: false
     t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "tags"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
@@ -52,8 +51,8 @@ ActiveRecord::Schema.define(version: 2018_11_14_062147) do
     t.string "tag", null: false
     t.integer "post_id", null: false
     t.string "post_type", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "user_id"
     t.index ["post_id"], name: "index_tags_on_post_id"
     t.index ["post_type"], name: "index_tags_on_post_type"
@@ -65,8 +64,8 @@ ActiveRecord::Schema.define(version: 2018_11_14_062147) do
     t.string "password_digest", null: false
     t.string "session_token", null: false
     t.string "email", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
@@ -75,12 +74,10 @@ ActiveRecord::Schema.define(version: 2018_11_14_062147) do
   create_table "votes", force: :cascade do |t|
     t.integer "vote_type", null: false
     t.integer "post_id", null: false
-    t.string "post_type", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "user_id"
     t.index ["post_id"], name: "index_votes_on_post_id"
-    t.index ["post_type"], name: "index_votes_on_post_type"
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
